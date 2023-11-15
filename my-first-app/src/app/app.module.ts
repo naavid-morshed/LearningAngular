d1754abfa12fd7c909d056a0c0c99b1fa28d6d70
Naavid Morshed <naavid.m@gmail.com>
2023-11-15 19:09:11 +0600

    basic done

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,13 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import { FormComponent } from './form/form.component';
-import { MultipleTypesOfSelectorsComponent } from './multiple-types-of-selectors/multiple-types-of-selectors.component';
-import { ButtonTestComponent } from './button-test/button-test.component';
+import {FormComponent} from './form/form.component';
+import {MultipleTypesOfSelectorsComponent} from './multiple-types-of-selectors/multiple-types-of-selectors.component';
+import {ButtonTestComponent} from './button-test/button-test.component';
 import {FormsModule} from "@angular/forms";
-import { Assignment2Component } from './assignment2/assignment2.component';
-import { NgIfConditionComponent } from './ng-if-condition/ng-if-condition.component';
-import { Assignment3Component } from './assignment3/assignment3.component';
+import {Assignment2Component} from './assignment2/assignment2.component';
+import {NgIfConditionComponent} from './ng-if-condition/ng-if-condition.component';
+import {Assignment3Component} from './assignment3/assignment3.component';
 
 @NgModule({
   declarations: [
