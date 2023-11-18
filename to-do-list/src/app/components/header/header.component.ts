9827efc74cffe4954036c0a9c6e39dd718a42890
naavid-morshed <naavid.m@gmail.com>
2023-11-18 22:02:46 +0600

    no msg

--- to-do-list/src/app/components/header/header.component.ts
+++ to-do-list/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CustomButtonComponent} from "../custom-button/custom-button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustomButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
