237db47e99415361b6a7b9598de40a7271c6f35c
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-08-12 09:16:19 +0600

    learned property binding, learned how to call components inside another component, looping is not working as intended. will fix asap

--- cooking-app/src/app/app.component.ts
+++ cooking-app/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {UserComponent} from "./user/user.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, UserComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cooking-app';
+    title = 'cooking-app';
+
+    users: { name: string; id: number; avatar: string }[] = [
+        {
+            id: 1,
+            name: "John",
+            avatar: "1"
+        }, {
+            id: 2,
+            name: "Doe",
+            avatar: "2"
+        }, {
+            id: 3,
+            name: "Jane",
+            avatar: "3"
+        },
+    ]
+
+    onSelectUser(id: number) {
+        console.log(id)
+    }
 }
