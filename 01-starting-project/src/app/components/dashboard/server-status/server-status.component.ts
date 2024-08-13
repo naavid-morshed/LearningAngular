7ac7568e9285863998796220aea078afccaf1571
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-08-13 12:21:52 +0600

    components modularized

--- 01-starting-project/src/app/components/dashboard/server-status/server-status.component.ts
+++ 01-starting-project/src/app/components/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-
+  @Input({required: true}) currentStatus!: string;
 }
